f2e6826c6056e1b3b1f00ca9f8bfa6f843788bac
petersamson-kingori <peters.kingori@student.moringaschool.com>
2023-04-29 22:28:15 +0300

    completed

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -1,2 +1,8 @@
 class Plant < ApplicationRecord
-end
+    validates :name, presence: true
+    validates :image, presence: true
+    validates :price, presence: true, numericality: { greater_than: 0 }
+  
+    # ... any additional code
+  end
+  
